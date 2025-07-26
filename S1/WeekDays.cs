8987fb2406c72561b2bf64074c6eec1b30681263
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-07-26 22:45:19 +0300

    Q1

---
+++ S1/WeekDays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1
+{
+    internal enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
